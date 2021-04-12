47b561c25ea8adf2564f3aa364297699f6517ee9
tatamiya <tamyuji1002@gmail.com>
2021-04-12 20:14:06 +0900

    Narrowing: the rest

--- narrowing.js
+++ narrowing.js
@@ -42,3 +42,109 @@ console.log(multiplyAll([0, 2, 5], 8));
 console.log(multiplyAll([], 8));
 console.log(multiplyAll(null, 8));
 console.log(multiplyAll(undefined, 8));
+// Equality narrowing
+function example(x, y) {
+    console.log(x, y, ":");
+    if (x === y) {
+        console.log(x.toUpperCase());
+        console.log(y.toLowerCase());
+    }
+    else {
+        console.log(x);
+        console.log(y);
+    }
+}
+example("hoge", "fuga");
+example(1, true);
+example("hoge", "hoge");
+example("hoge", true);
+function printAll2(strs) {
+    if (strs !== null) {
+        if (typeof strs === "object") {
+            for (var _i = 0, strs_2 = strs; _i < strs_2.length; _i++) {
+                var s = strs_2[_i];
+                console.log(s);
+            }
+        }
+        else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+printAll2("hoge");
+printAll2(["hoge", "fuga", "piyo"]);
+printAll2(null);
+printAll2([]);
+function multiplyValue(container, factor) {
+    if (container.value != null) {
+        console.log(container.value * factor);
+    }
+}
+multiplyValue({ value: 1 }, 3);
+multiplyValue({ value: null }, 3);
+multiplyValue({ value: undefined }, 3);
+// instanceof narrowing
+function logValue(x) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    }
+    else {
+        console.log(x.toUpperCase());
+    }
+}
+logValue("hoge");
+logValue(Date());
+// Assignments
+var x = Math.random() < 0.5 ? 10 : "hello, world!"; // string | number
+x = 1;
+console.log(x);
+x = "goodbye!";
+console.log(x);
+// x = true; Error!
+// Control flow analysis
+function example2() {
+    var x;
+    x = Math.random() < 0.5;
+    console.log(x);
+    if (Math.random() < 0.5) {
+        x = "hello";
+        console.log(x);
+    }
+    else {
+        x = 100;
+        console.log(x);
+    }
+    return x;
+}
+example2();
+//interface Shape {
+//    kind: "circle" | "square";
+//    radius?: number;
+//    sideLength?: number;
+//}
+//function getAreaError(shape: Shape) {
+//    return Math.PI * shape.radius ** 2;
+//}
+//type Shape = Circle | Square | Triangle;
+function getCircleArea(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * Math.pow(shape.radius, 2);
+    }
+}
+console.log(getCircleArea({ kind: "circle", radius: 3 }));
+//console.log(getArea({ kind: "circle", sideLength: 3 }));
+console.log(getCircleArea({ kind: "square", sideLength: 3 }));
+function getArea(shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * Math.pow(shape.radius, 2);
+        case "square":
+            return Math.pow(shape.sideLength, 2);
+        default:
+            var _exhaustiveCheck = shape;
+            return _exhaustiveCheck;
+    }
+}
+console.log(getArea({ kind: "circle", radius: 3 }));
+//console.log(getArea({ kind: "circle", sideLength: 3 }));
+console.log(getArea({ kind: "square", sideLength: 3 }));
--- narrowing.ts
+++ narrowing.ts
@@ -46,3 +46,131 @@ console.log(multiplyAll([], 8));
 console.log(multiplyAll(null, 8));
 console.log(multiplyAll(undefined, 8));
 
+// Equality narrowing
+function example(x: string | number, y: string | boolean) {
+    console.log(x, y, ":");
+    if (x === y) {
+
+        console.log(x.toUpperCase());
+        console.log(y.toLowerCase());
+    } else {
+        console.log(x);
+        console.log(y)
+    }
+}
+example("hoge", "fuga");
+example(1, true);
+example("hoge", "hoge");
+example("hoge", true);
+
+function printAll2(strs: string | string[] | null) {
+    if (strs !== null) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        } else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
+printAll2("hoge");
+printAll2(["hoge", "fuga", "piyo"]);
+printAll2(null);
+printAll2([]);
+
+interface Container {
+    value: number | null | undefined;
+}
+function multiplyValue(container: Container, factor: number) {
+    if (container.value != null) {
+        console.log(container.value * factor);
+    }
+}
+multiplyValue({ value: 1 }, 3);
+multiplyValue({ value: null }, 3);
+multiplyValue({ value: undefined }, 3);
+
+// instanceof narrowing
+function logValue(x: Date | string) {
+    if (x instanceof Date) {
+        console.log(x.toUTCString());
+    } else {
+        console.log(x.toUpperCase());
+    }
+}
+logValue("hoge");
+logValue(Date());
+
+// Assignments
+let x = Math.random() < 0.5 ? 10 : "hello, world!"; // string | number
+x = 1;
+console.log(x);
+x = "goodbye!";
+console.log(x);
+// x = true; Error!
+
+// Control flow analysis
+function example2() {
+    let x: string | number | boolean;
+    x = Math.random() < 0.5;
+    console.log(x);
+
+    if (Math.random() < 0.5) {
+        x = "hello";
+        console.log(x);
+    } else {
+        x = 100;
+        console.log(x);
+    }
+    return x;
+}
+example2();
+
+// Discriminated unions
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+interface Square {
+    kind: "square";
+    sideLength: number;
+}
+interface Triangle {
+    kind: "triangle";
+    sideLenght: number;
+}
+type Shape = Circle | Square;
+//interface Shape {
+//    kind: "circle" | "square";
+//    radius?: number;
+//    sideLength?: number;
+//}
+//function getAreaError(shape: Shape) {
+//    return Math.PI * shape.radius ** 2;
+//}
+//type Shape = Circle | Square | Triangle;
+function getCircleArea(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+}
+console.log(getCircleArea({ kind: "circle", radius: 3 }));
+//console.log(getArea({ kind: "circle", sideLength: 3 }));
+console.log(getCircleArea({ kind: "square", sideLength: 3 }));
+
+function getArea(shape: Shape) {
+    switch (shape.kind) {
+        case "circle":
+            return Math.PI * shape.radius ** 2;
+        case "square":
+            return shape.sideLength ** 2;
+        default:
+            const _exhaustiveCheck: never = shape;
+            return _exhaustiveCheck;
+    }
+}
+console.log(getArea({ kind: "circle", radius: 3 }));
+//console.log(getArea({ kind: "circle", sideLength: 3 }));
+console.log(getArea({ kind: "square", sideLength: 3 }));
+
