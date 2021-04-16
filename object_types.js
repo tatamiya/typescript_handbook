1af8d1caeee9a6d84c71119264b7a2881d0cc316
tatamiya <tamyuji1002@gmail.com>
2021-04-16 20:29:22 +0900

    Object Types

--- object_types.js
+++ object_types.js
@@ -33,3 +33,49 @@ var boxA = { contents: "hello" };
 console.log(boxA.contents);
 var boxB = { contents: "world" };
 console.log(boxB.contents);
+function doStuff(values) {
+    var copy = values.slice();
+    console.log("The first value is " + values[0]);
+    // Error!: values.push("hello!");
+}
+doStuff(["hoge", "fuga", "piyo"]);
+var roArray = ["red", "green", "blue"];
+// Impossible: new ReadonlyArray("red", "green", "blue");
+var x0 = [];
+var y0 = [];
+x0 = y0;
+function doSomethingWithTuple(pair) {
+    var a = pair[0];
+    var b = pair[1];
+    // Error!: const c = pair[2];
+    console.log(a, b);
+}
+doSomethingWithTuple(["hello", 42]);
+function setCoordinate(coord) {
+    var x = coord[0], y = coord[1], z = coord[2];
+    console.log("Provided coordinates had " + coord.length + " dimensions.");
+}
+// setCoordinate([1]);
+setCoordinate([1, 2]);
+setCoordinate([1, 2, 3]);
+var abn0 = ["hello", 0];
+var abn1 = ["hello", false, 0];
+var abn2 = ["hello", true, true, false, 0];
+// const abn3: StringBooleansNumber = ["hello", true, "hoge"];
+// const abn4: StringBooleansNumber = ["hello", "hoge", true, 0];
+// const abn5: StringBooleansNumber = ["hello", true];
+function distanceFromOrigin(coord) {
+    //let x = coord[0];
+    //let y = coord[1];
+    var x = coord[0], y = coord[1];
+    // Error!: coord[0] = 100;
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+console.log(distanceFromOrigin([0, 3]));
+function distanceFromOrigin2(_a) {
+    var x = _a[0], y = _a[1];
+    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+}
+var point = [3, 4]; // readonly
+console.log(distanceFromOrigin(point));
+// Error! Not assignable!: distanceFromOrigin2(point);
--- object_types.ts
+++ object_types.ts
@@ -98,3 +98,64 @@ let boxA: Box<string> = { contents: "hello" };
 console.log(boxA.contents);
 let boxB: StringBox = { contents: "world" };
 console.log(boxB.contents);
+
+function doStuff(values: ReadonlyArray<string>) {
+    const copy = values.slice();
+    console.log(`The first value is ${values[0]}`);
+
+    // Error!: values.push("hello!");
+}
+doStuff(["hoge", "fuga", "piyo"]);
+
+const roArray: ReadonlyArray<string> = ["red", "green", "blue"];
+// Impossible: new ReadonlyArray("red", "green", "blue");
+
+let x0: readonly string[] = [];
+let y0: string[] = [];
+x0 = y0;
+// not asiginable!: y0 = x0;
+
+type StringNumberPair = [string, number];
+function doSomethingWithTuple(pair: StringNumberPair) {
+    const a = pair[0];
+    const b = pair[1];
+    // Error!: const c = pair[2];
+    console.log(a, b);
+}
+doSomethingWithTuple(["hello", 42]);
+// doSomethingWithTuple([4, 5]);
+// doSomethingWithTuple(["hello", 42, 0]);
+
+type Either2dOr3d = [number, number, number?];
+function setCoordinate(coord: Either2dOr3d) {
+    const [x, y, z] = coord;
+    console.log(`Provided coordinates had ${coord.length} dimensions.`);
+}
+// setCoordinate([1]);
+setCoordinate([1, 2]);
+setCoordinate([1, 2, 3]);
+// setCoordinate([1, 2, 3, 4]);
+
+type StringBooleansNumber = [string, ...boolean[], number];
+const abn0: StringBooleansNumber = ["hello", 0];
+const abn1: StringBooleansNumber = ["hello", false, 0];
+const abn2: StringBooleansNumber = ["hello", true, true, false, 0];
+// const abn3: StringBooleansNumber = ["hello", true, "hoge"];
+// const abn4: StringBooleansNumber = ["hello", "hoge", true, 0];
+// const abn5: StringBooleansNumber = ["hello", true];
+
+function distanceFromOrigin(coord: readonly [number, number]) {
+    //let x = coord[0];
+    //let y = coord[1];
+    let [x, y] = coord;
+    // Error!: coord[0] = 100;
+    return Math.sqrt(x ** 2 + y ** 2);
+}
+console.log(distanceFromOrigin([0, 3]));
+
+function distanceFromOrigin2([x, y]: [number, number]) {
+    return Math.sqrt(x ** 2 + y ** 2);
+}
+let point = [3, 4] as const; // readonly
+console.log(distanceFromOrigin(point));
+// Error! Not assignable!: distanceFromOrigin2(point);
