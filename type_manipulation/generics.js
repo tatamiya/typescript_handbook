18de39e16bc2295d305da80e9e9dfca843a1da10
tatamiya <tamyuji1002@gmail.com>
2021-04-20 20:26:47 +0900

    Type Manipulation: Generics

--- type_manipulation/generics.js
+++ type_manipulation/generics.js
@@ -16,6 +16,42 @@ console.log(loggingIdentity([1, "hoge", 3]));
 var myIdentity1 = identity;
 var myIdentity2 = identity;
 var myIdentity3 = identity;
+var myIdentity4 = identity;
+console.log(myIdentity4("Hello, World!"));
+console.log(myIdentity4(1));
 var myIdentityNumber = identity;
+// Error! console.log(myIdentityNumber("Hello, World!"));
 console.log(myIdentityNumber(1));
-console.log(myIdentityNumber("Hello, World!"));
+// Generic Classes
+var GenericNumber = /** @class */ (function () {
+    function GenericNumber() {
+    }
+    return GenericNumber;
+}());
+var myGenericNumber = new GenericNumber();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+    return x + y;
+};
+console.log(myGenericNumber.add(10, 20));
+// Error!: console.log(myGenericNumber.add(10, "test"));
+var stringNumeric = new GenericNumber();
+stringNumeric.zeroValue = "";
+stringNumeric.add = function (x, y) {
+    return x + y;
+};
+console.log(stringNumeric.add(stringNumeric.zeroValue, "test"));
+function loggingIdentity2(args) {
+    console.log(args.length);
+    return args;
+}
+// Error!: loggingIdentity2(3);
+loggingIdentity2([1, 3, 5]);
+loggingIdentity2({ length: 10, value: 3 });
+// Using Type Parameters in Generic Constraints
+function getProperty(obj, key) {
+    return obj[key];
+}
+var x1 = { a: 1, b: 2, c: 3, d: 4 };
+console.log(getProperty(x1, "a"));
+// Error!: getProperty(x1, "m");
--- type_manipulation/generics.ts
+++ type_manipulation/generics.ts
@@ -30,3 +30,48 @@ interface GenericIdentityFn2<Type> {
 let myIdentityNumber: GenericIdentityFn2<number> = identity;
 // Error! console.log(myIdentityNumber("Hello, World!"));
 console.log(myIdentityNumber(1));
+
+// Generic Classes
+class GenericNumber<NumType> {
+    zeroValue: NumType;
+    add: (x: NumType, y: NumType) => NumType;
+}
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) {
+    return x + y;
+}
+console.log(myGenericNumber.add(10, 20));
+// Error!: console.log(myGenericNumber.add(10, "test"));
+
+let stringNumeric = new GenericNumber<string>();
+stringNumeric.zeroValue = "";
+stringNumeric.add = function (x, y) {
+    return x + y;
+}
+console.log(stringNumeric.add(stringNumeric.zeroValue, "test"));
+
+// Generic Constraints
+interface Lengthwise {
+    length: number;
+}
+function loggingIdentity2<Type extends Lengthwise>(args: Type): Type {
+    console.log(args.length);
+    return args;
+}
+// Error!: loggingIdentity2(3);
+loggingIdentity2([1, 3, 5]);
+loggingIdentity2({ length: 10, value: 3 });
+
+// Using Type Parameters in Generic Constraints
+function getProperty<Type, Key extends keyof Type>(obj: Type, key: Key) {
+    return obj[key];
+}
+let x1 = { a: 1, b: 2, c: 3, d: 4 };
+console.log(getProperty(x1, "a"));
+// Error!: getProperty(x1, "m");
+
+// Using Class Types in Generics
+function create<Type>(c: { new(): Type }): Type {
+    return new c();
+}
