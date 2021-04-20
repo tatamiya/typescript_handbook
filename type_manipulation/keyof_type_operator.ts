d4f1d41b4f612e019789c07a6bec0c6f506c18f9
tatamiya <tamyuji1002@gmail.com>
2021-04-20 20:27:05 +0900

    Type Manipulation: Typeof Type Operator

---
+++ type_manipulation/keyof_type_operator.ts
@@ -0,0 +1,7 @@
+type Point = { x: number; y: number };
+type P = keyof Point;
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish;
+
+type Mapish = { [k: string]: boolean };
+type M = keyof Mapish;
